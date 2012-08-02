a78a105e1db3b63aebb4c4927779d3d18d360ccb
Nils Gemeinhardt <git@nilsgemeinhardt.de>
2012-08-02 21:58:25 +0200

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+## packages
+
+
+```bash
+sudo apt-get install postgresql libpq-dev libpq-dev postgresql-devel libssl-dev openssl zlib1g-dev postgresql-client
+```
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BerlinerHoefe::Application.config.secret_token = '095c17b1821716ba1cd07de0c78bb68ebd70a012fab4e1759713dd7e084aac63f61982294ebbdc03bed8520b42cac0444b10fed88be8d36c46d4b0b47797c0f4'
