82aa6d996be35d5b2d9a04dd4b4dd3d260d547d8
Nils Gemeinhardt <git@nilsgemeinhardt.de>
2013-04-01 11:11:46 +0200

    change url structure

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,8 +1,17 @@
 class LocationsController < ApplicationController
 
+  def index
+    @locations = Location.published.for_map
+    @newest = @locations.newest
+    @body_id = 'root'
+  end
+
   def show
     @location = Location.find(params[:id])
     @nearby = @location.nearbys_for_map
   end
 
+  def redirect
+    redirect_to action: 'show', id: params[:id], status: 301
+  end
 end
--- app/controllers/root_controller.rb
+++
@@ -1,9 +0,0 @@
-class RootController < ApplicationController
-
-  def index
-    @locations = Location.published.for_map
-    @newest = @locations.newest
-    @body_id = 'root'
-  end
-
-end
