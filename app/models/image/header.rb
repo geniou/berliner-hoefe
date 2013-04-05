f81795ae9685eefa5a36cedea997f742652853cb
Nils Gemeinhardt <git@nilsgemeinhardt.de>
2013-04-05 14:46:37 +0200

    fix admin images and html

--- app/models/image/header.rb
+++ app/models/image/header.rb
@@ -1,6 +1,7 @@
 class Image::Header < Image
   has_attached_file :image,
     styles: {
+      thumb: "100x40>",
       small: "200x80>",
       large: "1200",
     }
--- app/models/image/slideshow.rb
+++ app/models/image/slideshow.rb
@@ -1,6 +1,7 @@
 class Image::Slideshow < Image
   has_attached_file :image,
     styles: {
+      thumb: "100x40>",
       small: "150x100#",
       large: "1000x666>",
     }
