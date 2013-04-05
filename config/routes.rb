bddbe36d58c7dfa422ed39f4b378c7d284aab8ec
Nils Gemeinhardt <git@nilsgemeinhardt.de>
2013-04-05 13:56:56 +0200

    set admin path root

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 BerlinerHoefe::Application.routes.draw do
-
-  get '/hof/:id' => 'locations#redirect'
-  get '/:id' => 'locations#show', as: 'location'
-
-  root :to => 'locations#index'
-
   namespace :admin do
     resources :locations do
       collection do
         post 'massoperation'
       end
     end
+    root to: 'locations#index'
   end
+
+  get '/hof/:id' => 'locations#redirect'
+  get '/:id' => 'locations#show', as: 'location'
+
+  root to: 'locations#index'
 end
